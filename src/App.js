fabcf360b2920dad8c8c561677ef83feca9fb475
Angelika Kędzierska <kedzierska_angelika@o2.pl>
2019-03-16 12:52:47 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from 'react-mdl';
+import {Layout} from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World
-            <Button raised accent>Button</Button>
+   <Layout style={{background: 'url(/pic1.jpg) center / cover'}}></Layout>
 
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
       </div>
     );
   }
